7559a520895eaa52ec08b68659d615c1053c0970
Vitaliy <vitaliy.matiyash@alliancedata.com>
2020-01-05 13:49:37 -0500

    Remove default HTML

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'finapp'`, () => {
+  it(`should have as title 'Finapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('finapp');
+    expect(app.title).toEqual('Finapp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to finapp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Finapp - lookup a stock!');
   });
 });
