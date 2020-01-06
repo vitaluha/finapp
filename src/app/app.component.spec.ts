7c1188b584a3ceba3069514bb882f0e855f2919a
Vitaliy <vitaliy.matiyash@alliancedata.com>
2020-01-06 00:00:45 -0500

    Add temporary stockChange method on input change. Add Stock model

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { componentFactoryName } from '@angular/compiler';
+import { Stock } from './stock/stock.model';
+// import { Stock } from "./stock/stock.model";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -19,22 +22,42 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  describe('app setup', () => {
+    let fixture, app;
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.debugElement.componentInstance;
+    });
 
-  it(`should have as title 'Finapp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Finapp');
-  });
+    it('should create the app', () => {
+      expect(app).toBeTruthy();
+    });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Finapp - lookup a stock!');
+    it(`should have as title 'Finapp'`, () => {
+      expect(app.title).toEqual('Finapp');
+    });
+
+    it('should render title in a h1 tag', () => {
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('Finapp - lookup a stock!');
+    });
+
+    describe('"stockChange"', () => {
+      it('should set stock symbol', () => {
+        const mockEvent = {
+          target: {
+            value: 'FB'
+          }
+        };
+        app.stockChange(mockEvent);
+
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h2').textContent).toContain(
+          '10'
+          );
+        });
+      });
+    });
   });
-});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Stock } from './stock/stock.model';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "Finapp";
+  title = 'Finapp';
+  stockName = '';
+  stock: Stock;
+
+  stockChange(event) {
+    if (!this.stock) {
+      this.stock = new Stock(event.target.value)
+    }
+    this.stock.symbol = event.target.value;
+    // Hardcode to 10
+    this.stock.price = 10;
+    console.log('Your stock is ' + this.stock.symbol + '\nPrice is: ' + this.stock.price);
+  }
 }
